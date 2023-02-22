044dd5b8d3577d491bfb6bfb43d8d91604d0dc30
Madi015 <madisaed2003@yahoo.com>
2023-02-22 14:24:41 +0100

    earsed all lines.

--- Program.cs
+++ Program.cs
@@ -4,28 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hej och välkommen till beräkningsprogrammet.");
-
-            int n = 2;
-            int sum = 0;
-            Console.WriteLine(" the squar is " + n * n);
-            for (int i = 0; i < 64; i++)
-            {
-                n = i * i;
-                sum = n * n;
-                Console.WriteLine(sum);
-
-            }
-
-            {
-                Console.WriteLine($"plus - addera två tal");
-                Console.WriteLine($"minus - subtrahera två tal");
-                Console.WriteLine($"gånger - multiplicera två tal");
-                Console.WriteLine($"delat - dividera två tal");
-                Console.WriteLine($"kvadrat - multiplicera tal med sig självt");
-                Console.WriteLine($"sluta - avsluta programmet");
-            }
+            Console.WriteLine("helloj");
         }
     }
 }
