2a8ec0c162e4b153aad353e6b2e14673d9d5f281
Madi015 <madisaed2003@yahoo.com>
2023-02-22 13:57:27 +0100

    one more line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hej och välkommen till beräkningsprogrammet.");
 
+            int n = 2;
+            Console.WriteLine(" the squar is " + n * n);
         }
     }
 }
