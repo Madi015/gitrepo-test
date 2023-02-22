6f020b08e185dfe9787d6b844b04d9e8517285c5
Madi015 <madisaed2003@yahoo.com>
2023-02-22 13:54:45 +0100

    added the second line.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hej och välkommen till beräkningsprogrammet.");
+
         }
     }
 }
